bd577adaff889a817c64075c4dc790c58c985f19
Sophia Hartland <sophart@bgsu.edu>
2026-07-07 19:27:38 +0000

    f

--- script.js
+++ script.js
@@ -9,8 +9,12 @@
 */
 
 // 1. Get the button and output area from the HTML
+const button = document.getElementbyID("CButt");
+let output = document.querySelector ("#output");
 
 // 2. Create a list of compliments to choose from
+const comp= ["You can do it!", "You're Amazing!", "The Sun is Shining Bright!", "You are so smart!", "You are great!", "You are loved!", "I believe in you!", "Don't worry! It'll be alright!", "It's great day for a pretzel!" ];
+
 
 // 3. Listen for a button click
 
